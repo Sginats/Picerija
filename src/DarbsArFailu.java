11130cba306f4eb004aecd8b1028da570750d635
Sginats <Senvaldsniks2008@gmail.com>
2026-01-14 09:30:16 +0200

    Pusots lai tiek pie darba

--- src/DarbsArFailu.java
+++ src/DarbsArFailu.java
@@ -1,4 +1,17 @@
-
+/*/
+ * ## ✅ TODO
+- [ ]Izveidot galveno **Picerija** klasi (nodrošina Picas izveidi, saglabāšanu un darbību izsaukšanu)
+- [ ]Izveidot Pica klasi ar atribūtiem (lielums, piedevas, mērce, cena, adrese, tālrunis, vārds)
+- [ ]Realizēt picas izveidi (izmērs, piedevas, mērce)
+- [ ]Ieviest piegādes informācijas ievadi (klienta vārds, adrese, tālrunis)
+- [ ]Nodrošināt ievades datu pārbaudi (teksts, skaitļi, tukšas vērtības)
+- [ ]Ieviest pasūtījumu labošanas funkcionalitāti
+- [ ]Aprēķināt pasūtījuma beigu summu
+- [ ]Izveidot DarbsArFailu klasi pasūtījumu saglabāšanai un nolasīšanai
+- [ ]Nodrošināt aktīvo un nodoto pasūtījumu apskati
+- [ ]Izveidot lietotāja saskarni ar JFrame
+- [ ]Apstrādāt kļūdas un nepareizas lietotāja darbības
+ */
 public class DarbsArFailu {
 
 }
